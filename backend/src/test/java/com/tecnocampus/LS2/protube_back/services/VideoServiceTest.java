682901a01e8968b602d0f0e8eaf968114e192149
Ferran <lobafe.0605@gmail.com>
2025-11-03 13:53:22 +0100

    test for request

--- backend/src/test/java/com/tecnocampus/LS2/protube_back/services/VideoServiceTest.java
+++ backend/src/test/java/com/tecnocampus/LS2/protube_back/services/VideoServiceTest.java
@@ -1,8 +1,11 @@
 package com.tecnocampus.LS2.protube_back.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tecnocampus.LS2.protube_back.repositories.IVideoFileRepository;
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -12,12 +15,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class VideoServiceTest {
 
 
-    VideoService videoService = new VideoService();
 
-    @Ignore
-    @Test
-    void shouldGoToFolderVideos() {
-        assertEquals(List.of(), videoService.getVideos());
-    }
+    //@Ignore
+    //@Test
+    //void shouldGoToFolderVideos() {
+      //  assertEquals(List.of(), videoService.getVideos());
+    //}
 
 }
